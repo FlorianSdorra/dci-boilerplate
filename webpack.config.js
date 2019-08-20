dfc076b4a6437f6d647a3ca0ff531abeb380e516
Itamar Givon <itamar.givon.dev@gmail.com>
2019-08-20 11:36:23 +0200

    Add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js/'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: path.join('scripts', 'bundle.js')
   },
   module: {
     rules: [
@@ -17,7 +18,22 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
 };
